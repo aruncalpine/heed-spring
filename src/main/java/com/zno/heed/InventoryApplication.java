aa158ea64d32b0e7d33ca9cf9e79570a9d5e93f8
diaz-johnson <diaz.johnson@calpinetech.com>
2023-07-25 18:45:44 +0530

    server deploy

--- src/main/java/com/zno/heed/InventoryApplication.java
+++ src/main/java/com/zno/heed/InventoryApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 @SpringBootApplication
 @EnableCaching
 @EnableAutoConfiguration(exclude={ValidationAutoConfiguration.class})
@@ -19,7 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class InventoryApplication extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(InventoryApplication.class, args);
+		SpringApplication.run(applicationClass, args);
 		System.out.println("---------------------------------------------------------------------------- ZNO - HEED - Started -----------------------------------------------------------------------------------------------");
 	}
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+ 
+    private static Class<InventoryApplication> applicationClass = InventoryApplication.class;
 }
+
